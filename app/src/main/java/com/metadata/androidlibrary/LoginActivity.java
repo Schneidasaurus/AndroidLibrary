d4a409facb6f05f584bfc2daa8c13811a5a283ad
paolee <ics3722016>
2017-04-06 20:55:30 -0500

    I added android codes for the login page and item list page, feel free to edit them if you guys could.

---
+++ app/src/main/java/com/metadata/androidlibrary/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.metadata.androidlibrary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        String password = "9874764";
+
+        Button b = (Button)findViewById(R.id.login_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
